2c79c8e21f494877bfce8a157c04759701fa132b
Mostafa Mahmoud Osman Sdeak <mostafa.mahmoud.osman.sdeak@gmail.com>
2026-08-31 03:57:53 +0300

    Add files via upload

---
+++ preprocessing/code/preprocess_da.py
@@ -0,0 +1,5 @@
+def drop_columns(df, columns):
+    return df.drop(columns=columns)
+def get_data_summary(df: pd.DataFrame)->pd.DataFrame:
+        
+    return pd.DataFrame({"Dtypes ": df.dtypes, "N_unique ":df.n_unique}).T
